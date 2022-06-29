08974fcdb7e2a6881c90a3478152192d750c2729
ciro <cirovitale@pm.me>
2022-06-29 11:58:44 +0200

    update TesterServlet

--- gamePlatformSite/src/main/java/it/unisa/gp/control/TesterServlet.java
+++ gamePlatformSite/src/main/java/it/unisa/gp/control/TesterServlet.java
@@ -3,20 +3,17 @@ package it.unisa.gp.control;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
-
-
+import java.time.LocalDate;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
-
 import java.util.Collection;
-
-import it.unisa.gp.model.DAO.AbbonamentoDS;
-import it.unisa.gp.model.bean.AbbonamentoBean;
-import it.unisa.gp.model.interfaceDS.Abbonamento;
+import it.unisa.gp.model.DAO.AssistenteClientiDS;
+import it.unisa.gp.model.bean.AssistenteClientiBean;
+import it.unisa.gp.model.interfaceDS.AssistenteClienti;
 
 /**
  * Servlet implementation class TesterServlet
@@ -24,13 +21,13 @@ import it.unisa.gp.model.interfaceDS.Abbonamento;
 @WebServlet("/TesterServlet")
 public class TesterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TesterServlet() {
-        super();
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public TesterServlet() {
+		super();
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -38,60 +35,63 @@ public class TesterServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		out.println("<p> Ciao </p>");
-		
+
 		DataSource ds = (DataSource) getServletContext().getAttribute("DataSource");
-		Abbonamento abb = new AbbonamentoDS(ds);
-		AbbonamentoBean bean = new AbbonamentoBean("alfredoo cuozzo",30,50);
-		
-		
-		try {
-			abb.doSave(bean);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+		AssistenteClienti ass = new AssistenteClientiDS(ds);
+		AssistenteClientiBean bean = new AssistenteClientiBean("VTLCRIH703H10DEd", "Ciro", "Vitale", LocalDate.of(2001,06,12),
+				"ciro@email.it", "passwordTest", 10000);
+
 		/*
 		try {
-			abb.doUpdate(bean,45,67);
+			ass.doSave(bean);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+		*/
+
+		/*
+		try {
+			ass.doUpdate(bean,"Alfredo", "Cuozzo", LocalDate.of(2001,06,13),
+					"alf@email.it", "passwordTest1", 100001, 1, 5);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		*/
+		/*
 		// funziona
 		try {
-			abb.doDelete(bean.getNomeUnivoco());
+			ass.doDelete(bean.getCodiceFiscale());
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+		*/
+		/*
 		// funziona
 		try {
-			bean = abb.doRetrieveByKey("alfredo cuozzo");
+			bean = ass.doRetrieveByKey("VTLCRI01H12H703D");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		out.println(bean.toString()); 	//print di debug
-		
-		
+		*/
+		/*
 		// funziona
-		Collection<AbbonamentoBean> abbonament = null;
+		Collection<AssistenteClientiBean> abbonament = null;
 
 		try {
-			abbonament = (Collection<AbbonamentoBean>) abb.doRetrieveAll("NOME_UNIVOCO ASC");
+			abbonament = (Collection<AssistenteClientiBean>) ass.doRetrieveAll(null);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		out.println(abbonament.toString()); 
-	*/
+		*/
 	}
 
 	/**
