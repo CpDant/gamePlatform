1b6432a8370dbc3aaa0c47d9cef75a79607a91f0
ciro <cirovitale@pm.me>
2022-06-29 11:57:46 +0200

    Implementazione DAO per tabella assistente_clienti

---
+++ gamePlatformSite/src/main/java/it/unisa/gp/model/DAO/AssistenteClientiDS.java
@@ -0,0 +1,213 @@
+package it.unisa.gp.model.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.LinkedList;
+import javax.sql.DataSource;
+import it.unisa.gp.model.bean.AssistenteClientiBean;
+import it.unisa.gp.model.interfaceDS.AssistenteClienti;
+
+public class AssistenteClientiDS implements AssistenteClienti {
+	private static final String TABLE_NAME = "assistente_clienti";
+	private DataSource ds = null;
+	
+	public AssistenteClientiDS(DataSource ds) {
+		this.ds = ds;
+		
+		System.out.println("Creazione DataSource...");
+	}
+	
+	@Override
+	public void doSave(AssistenteClientiBean ass) throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStmt = null;
+		
+		String insertSQL = "INSERT INTO " + AssistenteClientiDS.TABLE_NAME
+				+ " (CODICE_FISCALE, NOME, COGNOME, DATA_NASCITA, RUOLO, EMAIL, PASS_WORD,"
+				+ "RETRIBUZIONE_ANNUALE, TICKET_DA_RISOLVERE, TICKET_RISOLTI) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		
+		try {
+			connection = ds.getConnection();
+			preparedStmt = connection.prepareStatement(insertSQL);
+			preparedStmt.setString(1, ass.getCodiceFiscale());
+			preparedStmt.setString(2, ass.getNome());
+			preparedStmt.setString(3, ass.getCognome());
+			preparedStmt.setDate(4, java.sql.Date.valueOf(ass.getDataNascita()));
+			preparedStmt.setString(5, ass.getRuolo());
+			preparedStmt.setString(6, ass.getEmail());
+			preparedStmt.setString(7, ass.getPassWord());
+			preparedStmt.setInt(8, ass.getRetribuzioneAnnuale());
+			preparedStmt.setInt(9, ass.getTicketDaRisolvere());
+			preparedStmt.setInt(10, ass.getTicketRisolti());
+
+			preparedStmt.executeUpdate();
+
+			connection.setAutoCommit(false);
+			connection.commit();
+		} finally {
+			try {
+				if (preparedStmt != null)
+					preparedStmt.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+	}
+
+	@Override
+	public void doUpdate(AssistenteClientiBean ass, String nome, String cognome, LocalDate dataNascita, String email,
+			String password, int retribuzioneAnnuale, int ticketDaRisolvere, int ticketRisolti) throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStmt = null;
+		
+		String updateSQL = "UPDATE " + AssistenteClientiDS.TABLE_NAME
+				+ " SET NOME = ?, COGNOME = ?, DATA_NASCITA = ?, EMAIL = ?, PASS_WORD = ?,"
+				+ "RETRIBUZIONE_ANNUALE = ?, TICKET_DA_RISOLVERE = ?, TICKET_RISOLTI = ?" + " WHERE CODICE_FISCALE = ?";
+		
+		try {
+			connection = ds.getConnection();
+			preparedStmt = connection.prepareStatement(updateSQL);
+			preparedStmt.setString(1, nome);
+			preparedStmt.setString(2, cognome);
+			preparedStmt.setDate(3, java.sql.Date.valueOf(dataNascita));
+			preparedStmt.setString(4, email);
+			preparedStmt.setString(5, password);
+			preparedStmt.setInt(6, retribuzioneAnnuale);
+			preparedStmt.setInt(7, ticketDaRisolvere);
+			preparedStmt.setInt(8, ticketRisolti);
+			preparedStmt.setString(9, ass.getCodiceFiscale());
+
+			preparedStmt.executeUpdate();
+
+			connection.setAutoCommit(false);
+			connection.commit();
+		} finally {
+			try {
+				if (preparedStmt != null)
+					preparedStmt.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+	}
+
+	@Override
+	public boolean doDelete(String codiceFiscale) throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStmt = null;
+
+		int result = 0;
+
+		String deleteSQL = "DELETE FROM " + AssistenteClientiDS.TABLE_NAME + " WHERE CODICE_FISCALE = ?";
+
+		try {
+			connection = ds.getConnection();
+			preparedStmt = connection.prepareStatement(deleteSQL);
+			preparedStmt.setString(1, codiceFiscale);
+
+			result = preparedStmt.executeUpdate();
+
+		} finally {
+			try {
+				if (preparedStmt != null)
+					preparedStmt.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+		return (result != 0);
+	}
+
+	@Override
+	public AssistenteClientiBean doRetrieveByKey(String codiceFiscale) throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStmt = null;
+
+		AssistenteClientiBean ass = new AssistenteClientiBean(null,null,null,null,null,null,0);
+
+		String selectSQL = "SELECT * FROM " + AssistenteClientiDS.TABLE_NAME + " WHERE CODICE_FISCALE = ?";
+
+		try {
+			connection = ds.getConnection();
+			preparedStmt = connection.prepareStatement(selectSQL);
+			preparedStmt.setString(1, codiceFiscale);
+
+			ResultSet rs = preparedStmt.executeQuery();
+
+			while (rs.next()) {
+				ass.setCodiceFiscale(rs.getString("CODICE_FISCALE"));
+				ass.setNome(rs.getString("NOME"));
+				ass.setCognome(rs.getString("COGNOME"));
+				ass.setDataNascita((rs.getDate("DATA_NASCITA")).toLocalDate());
+				ass.setEmail(rs.getString("EMAIL"));
+				ass.setPassWord(rs.getString("PASS_WORD"));
+				ass.setRetribuzioneAnnuale(rs.getInt("RETRIBUZIONE_ANNUALE"));
+				ass.setTicketDaRisolvere(rs.getInt("TICKET_DA_RISOLVERE"));
+				ass.setTicketRisolti(rs.getInt("TICKET_RISOLTI"));
+			}
+
+		} finally {
+			try {
+				if (preparedStmt != null)
+					preparedStmt.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+		return ass;
+	}
+
+	@Override
+	public Collection<AssistenteClientiBean> doRetrieveAll(String order) throws SQLException {
+		Connection connection = null;
+		PreparedStatement preparedStmt = null;
+
+		Collection<AssistenteClientiBean> products = new LinkedList<AssistenteClientiBean>();
+
+		String selectSQL = "SELECT * FROM " + AssistenteClientiDS.TABLE_NAME;
+		
+
+		if (order != null && !order.equals("")) {
+			selectSQL += " ORDER BY " + order;
+		}
+
+		try {
+			connection = ds.getConnection();
+			preparedStmt = connection.prepareStatement(selectSQL);
+
+			ResultSet rs = preparedStmt.executeQuery();
+			while (rs.next()) {
+				AssistenteClientiBean ass = new AssistenteClientiBean(null,null,null,null,null,null,0);
+				ass.setCodiceFiscale(rs.getString("CODICE_FISCALE"));
+				ass.setNome(rs.getString("NOME"));
+				ass.setCognome(rs.getString("COGNOME"));
+				ass.setDataNascita((rs.getDate("DATA_NASCITA")).toLocalDate());
+				ass.setEmail(rs.getString("EMAIL"));
+				ass.setPassWord(rs.getString("PASS_WORD"));
+				ass.setRetribuzioneAnnuale(rs.getInt("RETRIBUZIONE_ANNUALE"));
+				ass.setTicketDaRisolvere(rs.getInt("TICKET_DA_RISOLVERE"));
+				ass.setTicketRisolti(rs.getInt("TICKET_RISOLTI"));
+				products.add(ass);
+			}
+
+		} finally {
+			try {
+				if (preparedStmt != null)
+					preparedStmt.close();
+			} finally {
+				if (connection != null)
+					connection.close();
+			}
+		}
+		return products;
+	}
+
+}
---
+++ gamePlatformSite/src/main/java/it/unisa/gp/model/interfaceDS/AssistenteClienti.java
@@ -0,0 +1,20 @@
+package it.unisa.gp.model.interfaceDS;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.Collection;
+
+import it.unisa.gp.model.bean.AssistenteClientiBean;
+
+public interface AssistenteClienti {
+	public void doSave(AssistenteClientiBean ass) throws SQLException;
+	
+	public void doUpdate(AssistenteClientiBean sup, String nome, String cognome, LocalDate dataNascita,
+			String email, String password, int retribuzioneAnnuale, int ticketDaRisolvere, int ticketRisolti) throws SQLException;
+
+	public boolean doDelete(String codiceFiscale) throws SQLException;
+
+	public AssistenteClientiBean doRetrieveByKey(String codiceFiscale) throws SQLException;
+	
+	public Collection<AssistenteClientiBean> doRetrieveAll(String order) throws SQLException;
+}
