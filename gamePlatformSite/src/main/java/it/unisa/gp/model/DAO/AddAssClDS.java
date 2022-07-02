14f9da5f900c2f1b636c69c207fc2206dc681228
ciro <cirovitale@pm.me>
2022-07-02 20:25:26 +0200

    Creazione classi DAO senza implementazione per AddAssCl

---
+++ gamePlatformSite/src/main/java/it/unisa/gp/model/DAO/AddAssClDS.java
@@ -0,0 +1,7 @@
+package it.unisa.gp.model.DAO;
+
+import it.unisa.gp.model.interfaceDS.AddAssCl;
+
+public class AddAssClDS implements AddAssCl {
+
+}
---
+++ gamePlatformSite/src/main/java/it/unisa/gp/model/interfaceDS/AddAssCl.java
@@ -0,0 +1,5 @@
+package it.unisa.gp.model.interfaceDS;
+
+public interface AddAssCl {
+
+}
