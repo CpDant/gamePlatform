0325f1a45bbfef00eb634f69cc188f65e3cd735d
ciro <cirovitale@pm.me>
2022-07-09 19:24:10 +0200

    Eliminazione Servlet non necessarie e modifica PopolamentoDSServlet.java

--- gamePlatformSite/src/main/java/it/unisa/gp/control/PopolamentoDBServlet.java
+++ gamePlatformSite/src/main/java/it/unisa/gp/control/PopolamentoDBServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,74 +11,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
-import java.util.Collection;
 
-import it.unisa.gp.model.DAO.AbbonamentoDS;
-import it.unisa.gp.model.DAO.AcqContieneAbbDS;
-import it.unisa.gp.model.DAO.AcqContieneVidDS;
-import it.unisa.gp.model.DAO.AcquistiDS;
 import it.unisa.gp.model.DAO.AddAbbDS;
 import it.unisa.gp.model.DAO.AddAssClDS;
-import it.unisa.gp.model.DAO.AssistenteClientiDS;
 import it.unisa.gp.model.DAO.ClientiDS;
-import it.unisa.gp.model.DAO.FatturaDS;
-import it.unisa.gp.model.DAO.ModAbbDS;
-import it.unisa.gp.model.DAO.ModVideogDS;
-import it.unisa.gp.model.DAO.RemInAbbDS;
-import it.unisa.gp.model.DAO.RemSupVidDS;
-import it.unisa.gp.model.DAO.RemVideogDS;
 import it.unisa.gp.model.DAO.SoftwareHouseDS;
-import it.unisa.gp.model.DAO.SupervisoreVideogiochiDS;
 import it.unisa.gp.model.DAO.TelefonoDS;
 import it.unisa.gp.model.DAO.AddInAbbDS;
 import it.unisa.gp.model.DAO.AddSupVidDS;
 import it.unisa.gp.model.DAO.AddVideogDS;
 import it.unisa.gp.model.DAO.AdministratorsDS;
-import it.unisa.gp.model.DAO.VideogiocoDS;
-import it.unisa.gp.model.bean.AbbonamentoBean;
-import it.unisa.gp.model.bean.AcqContieneAbbBean;
-import it.unisa.gp.model.bean.AcqContieneVidBean;
-import it.unisa.gp.model.bean.AcquistiBean;
 import it.unisa.gp.model.bean.AddAbbBean;
 import it.unisa.gp.model.bean.AddAssClBean;
-import it.unisa.gp.model.bean.AssistenteClientiBean;
 import it.unisa.gp.model.bean.ClientiBean;
-import it.unisa.gp.model.bean.ModAbbBean;
-import it.unisa.gp.model.bean.ModVideogBean;
-import it.unisa.gp.model.bean.RemInAbbBean;
-import it.unisa.gp.model.bean.RemSupVidBean;
-import it.unisa.gp.model.bean.RemVideogBean;
 import it.unisa.gp.model.bean.SoftwareHouseBean;
-import it.unisa.gp.model.bean.SupervisoreVideogiochiBean;
 import it.unisa.gp.model.bean.TelefonoBean;
 import it.unisa.gp.model.bean.AddInAbbBean;
 import it.unisa.gp.model.bean.AddSupVidBean;
 import it.unisa.gp.model.bean.AddVideogBean;
 import it.unisa.gp.model.bean.AdministratorsBean;
-import it.unisa.gp.model.bean.VideogiocoBean;
 import it.unisa.gp.model.bean.VideogiocoBean.Pegi;
-import it.unisa.gp.model.interfaceDS.Abbonamento;
-import it.unisa.gp.model.interfaceDS.AcqContieneAbb;
-import it.unisa.gp.model.interfaceDS.AcqContieneVid;
-import it.unisa.gp.model.interfaceDS.Acquisti;
 import it.unisa.gp.model.interfaceDS.AddAbb;
 import it.unisa.gp.model.interfaceDS.AddAssCl;
-import it.unisa.gp.model.interfaceDS.AssistenteClienti;
 import it.unisa.gp.model.interfaceDS.Clienti;
-import it.unisa.gp.model.interfaceDS.Fattura;
-import it.unisa.gp.model.interfaceDS.ModAbb;
-import it.unisa.gp.model.interfaceDS.ModVideog;
-import it.unisa.gp.model.interfaceDS.RemInAbb;
-import it.unisa.gp.model.interfaceDS.RemSupVid;
-import it.unisa.gp.model.interfaceDS.RemVideog;
 import it.unisa.gp.model.interfaceDS.SoftwareHouse;
-import it.unisa.gp.model.interfaceDS.SupervisoreVideogiochi;
 import it.unisa.gp.model.interfaceDS.Telefono;
 import it.unisa.gp.model.interfaceDS.AddInAbb;
 import it.unisa.gp.model.interfaceDS.AddSupVid;
 import it.unisa.gp.model.interfaceDS.AddVideog;
 import it.unisa.gp.model.interfaceDS.Administrators;
-import it.unisa.gp.model.interfaceDS.Videogioco;
 
 /**
  * Servlet implementation class PopolamentoDBServlet
@@ -117,7 +77,7 @@ public class PopolamentoDBServlet extends HttpServlet {
         Telefono telDS = new TelefonoDS(ds);
         
         
-        /*
+        
         // Administrators
         AdministratorsBean adminBean = new AdministratorsBean("CZZLRD76L31F205Y", "Alfredo", "Cuozzo", LocalDate.of(1976, 7, 31), "alfred@email.it", "acs9q8s6", 40000);
         
@@ -169,8 +129,6 @@ public class PopolamentoDBServlet extends HttpServlet {
         AddInAbbBean addInAbbBean3 = new AddInAbbBean("VRRMSM99S23F839O", "BVNMQ456", "Silver Pass");
         AddInAbbBean addInAbbBean4 = new AddInAbbBean("VRRMSM99S23F839O", "FD32SS16", "Silver Pass");
         
-<<<<<<< HEAD
-=======
         AddInAbbBean addInAbbBean5 = new AddInAbbBean("VRRMSM99S23F839O", "FD32SS16", "Gold Pass");
         AddInAbbBean addInAbbBean6 = new AddInAbbBean("VRRMSM99S23F839O", "PLO094FT", "Gold Pass");
         AddInAbbBean addInAbbBean7 = new AddInAbbBean("VRRMSM99S23F839O", "ACV458V1", "Gold Pass");
@@ -200,7 +158,6 @@ public class PopolamentoDBServlet extends HttpServlet {
         AddInAbbBean addInAbbBean30 = new AddInAbbBean("NDRNCL71A02F205D", "FCR787U0", "Platinum Pass");
         AddInAbbBean addInAbbBean31 = new AddInAbbBean("NDRNCL71A02F205D", "WDT712T8", "Platinum Pass");
         AddInAbbBean addInAbbBean32 = new AddInAbbBean("NDRNCL71A02F205D", "ACV345T1", "Platinum Pass");
-        */
         
         // Clienti e telefoni
         ClientiBean clientiBean1 = new ClientiBean("VTLCRI01H12H703D", "Ciro", "Vitale", LocalDate.of(2001, 6, 12), "cirovit@gmail.com", "as894c65a1", "pipita089", "via Roma 1");
@@ -219,7 +176,7 @@ public class PopolamentoDBServlet extends HttpServlet {
         
         // Aggiunta al DB
         try {
-        	/*
+        	
         	// Administrators
 			adminDS.doSave(adminBean);
 			
@@ -298,7 +255,7 @@ public class PopolamentoDBServlet extends HttpServlet {
         	addInAbbDS.doSave(addInAbbBean30);
         	addInAbbDS.doSave(addInAbbBean31);
         	addInAbbDS.doSave(addInAbbBean32);
-        	*/
+        	
         	// Clienti e telefoni
         	clientiDS.doSave(clientiBean1);
         	clientiDS.doSave(clientiBean2);
@@ -318,211 +275,7 @@ public class PopolamentoDBServlet extends HttpServlet {
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-        
-        /*
-        AcqContieneAbb aca = new AcqContieneAbbDS(ds);
-        AcqContieneVid acv = new AcqContieneVidDS(ds);
-        Acquisti acq = new AcquistiDS(ds);
-        Fattura fat = new FatturaDS(ds);
-                
-        int id = 123;
-        
-       
-        Abbonamento abb = new AbbonamentoDS(ds);
-        AbbonamentoBean abbBean1 = new AbbonamentoBean("xbox pass",30,15);
-        AbbonamentoBean abbBean2 = new AbbonamentoBean("playstation pass",30,25);
-
-        SoftwareHouse soft = new SoftwareHouseDS(ds);
-        SoftwareHouseBean softBean = new SoftwareHouseBean("ea sports", "usa", LocalDate.of(1995, 10, 3));
-
-        Videogioco vid = new VideogiocoDS(ds);
-        VideogiocoBean vidBean1 = new VideogiocoBean("asc5", "ea sports", "Fifa 21", 10000, 2015, 50, Pegi.tre);
-        VideogiocoBean vidBean2 = new VideogiocoBean("ads465", "ea sports", "rocket", 645, 2015, 60, Pegi.tre);
-        VideogiocoBean vidBean3 = new VideogiocoBean("cas6541", "ea sports", "ufc", 64544, 2015, 30, Pegi.tre);
-
-        SupervisoreVideogiochi sup = new SupervisoreVideogiochiDS(ds);
-        SupervisoreVideogiochiBean supBean = new SupervisoreVideogiochiBean("acs4csa45", "ciro", "Vitale", LocalDate.of(2001, 10, 3), "ciro@email", "passwrod", 51000);
-
-        AddInAbb add = new AddInAbbDS(ds);
-        AddInAbbBean addBean1 = new AddInAbbBean("acs4csa45", "asc5", "xbox pass");
-        AddInAbbBean addBean2 = new AddInAbbBean("acs4csa45", "ads465", "xbox pass");
-        AddInAbbBean addBean3 = new AddInAbbBean("acs4csa45", "cas6541", "xbox pass");
-        AddInAbbBean addBean4 = new AddInAbbBean("acs4csa45", "cas6541", "playstation pass");
-        AddInAbbBean addBean5 = new AddInAbbBean("acs4csa45", "asc5", "playstation pass"); 
-        
-        
-        
- 
-        try {
-        	
-        	abb.doSave(abbBean1);
-			abb.doSave(abbBean2);
-			soft.doSave(softBean);
-			vid.doSave(vidBean1);
-			vid.doSave(vidBean2);
-			vid.doSave(vidBean3);
-			sup.doSave(supBean);
-			add.doSave(addBean1);
-			add.doSave(addBean2);
-			add.doSave(addBean3);
-			add.doSave(addBean4);
-			add.doSave(addBean5);
-			
-			
-			
-		} catch (SQLException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-        
-		*/
-        
-    
-        
-        /*
-        RemInAbb remAbb = new RemInAbbDS(ds);
-        RemInAbbBean remBean = new RemInAbbBean("acs4csa45", "cas6541", "donato cuozzo");
-        
-        ModAbb modAbb = new ModAbbDS(ds);
-        ModAbbBean modBean = new ModAbbBean("acs4csa45", "alfredo cuozzo");
-        
-        AddSupVid addSupVid = new AddSupVidDS(ds);
-        AddSupVidBean addSupVidBean = new AddSupVidBean("mioCodiceFisc","ABCD123");
-        
-        RemSupVid remSupVid = new RemSupVidDS(ds);
-        RemSupVidBean remSupVidBean = new RemSupVidBean("acs4csa45","ABCD123");
-        
-        */
-
-        
-        //Tickets tick = new TicketsDS(ds)
-        
-        /*
-        ModAbb modAbb = new ModAbbDS(ds);
-        ModAbbBean bean1 = new ModAbbBean("acs4csa45", "alfredo cuozzo");
-        ModAbbBean bean2 = new ModAbbBean("acs4csa45", "donato cuozzo");
-        */
-        
-        /*
-        RemAssCl remAssCl = new RemAssClDS(ds);
-        RemAssClBean remAssClBean = new RemAssClBean("sballo","ABCD123");
-        */
-        
-        /*
-        Acquisti acq = new AcquistiDS(ds);
-        //AcquistiBean acqBean = new AcquistiBean(123,"CODICE","CJDEJFKMWOI3",30,70,LocalDateTime.of(2022,10,14,17,34,21),null,5333122);
-        AcqContieneVid acqVid = new AcqContieneVidDS(ds);
-        AcqContieneAbb acqAbb = new AcqContieneAbbDS(ds);
-        
-        RemVideog rem = new RemVideogDS(ds);
-        RemVideogBean beanR = new RemVideogBean("acs4csa45", "asc5");
-        
-        ModVideog modVid = new ModVideogDS(ds);
-        ModVideogBean modBean = new ModVideogBean("acs4csa45", "ads465");
-        
-        Fattura fat = new FatturaDS(ds);
-        
-        try {
-			acq.doSave(123,"GDFSAOIAFSJM", "CODICE", LocalDateTime.of(2022,10,14,17,34,21), 5333122L);
-			acqVid.doSave(123, "ads465");
-			acqVid.doSave(123, "asc5");
-			acqAbb.doSave(123, "xbox pass");
-			acq.doUpdate(123);
-			
-        	rem.doSave(beanR);
-        	modVid.doSave(modBean, "ads465", "rocket", 645, 2015, 100, Pegi.tre, false);        	
-			
-        	
-        	fat.doSave(123, LocalDateTime.now());
-        	
-        } catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        */
-        
-        
-        /*
-        AddVideog addVid = new AddVideogDS(ds);
-        AddVideogBean addVidBean1 = new AddVideogBean("berrfsdsd","ads4657");
-        AddVideogBean addVidBean2 = new AddVideogBean("berrfsdsd","xcas65412");
-        */
-        
-        /*
-        try {
-            /*
-            abb.doSave(abbBean1);
-            abb.doSave(abbBean2);
-            soft.doSave(softBean);
-            vid.doSave(vidBean1);
-            vid.doSave(vidBean2);
-            vid.doSave(vidBean3);
-            sup.doSave(supBean);
-            add.doSave(addBean1);
-            add.doSave(addBean2);
-            add.doSave(addBean3);
-            add.doSave(addBean4);
-            add.doSave(addBean5);
-        	
-            modAbb.doSave(bean1,"alfredo cuozzo",20,10);
-        	modAbb.doSave(bean2,"donato cuozzo",40,100);
-        	
-        	addAssCl.doSave(addAssBean, "sballo", "Marco", "Maffeo", LocalDate.of(2002,1,7), "banana33@gmail.com", "fragola86", 2);;
-        	
-        	acq.doSave(acqBean);
-        	acqVid.doSave(acqVidBean1);
-        	acqVid.doSave(acqVidBean2);
-        	acqAbb.doSave(acqAbbBean1);
-        	acqAbb.doSave(acqAbbBean2);
-        	addVid.doSave(addVidBean2);
-
-        	addAssCl.doSave(addAssBean, "zzasf654", "Luigi", "Consiglio", LocalDate.of(2001, 1, 1), "em", "passw", 5000);
-        	addVid.doSave(addVidBean2,"xcas65412", "ea sports", "moto gp", 645, 2020, 73, Pegi.sette);
-        	*/
-        	/*
-        	acq.doSave(id, "CJDEJFKMWOI3", "RISCATTOX", LocalDateTime.now(), 654851544566L);
-        	abb.doSave(contAbbBean1);
-        	abb.doSave(contAbbBean);
-        	vid.doSave(contVidBean);
-        	acq.doUpdate(id);
-        	
-        
-        	fat.doSave(id, LocalDateTime.now());
-        	
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        */
-        
-        
-        
-
-		/*
-		AddInAbbBean bean = null;
-		try {
-			bean = fat.doRetrieveByKey(123, 1);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		out.println(bean.toString()); 	//print di debug
-		*/
-		
-		/*
-		Collection<AddInAbbBean> abbonament = null;
-
-		try {
-			abbonament = (Collection<AddInAbbBean>) fat.doRetrieveAll(null);
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		out.println(abbonament.toString()); 
-		*/
-		 
+		}		 
 	}
 
 	/**
