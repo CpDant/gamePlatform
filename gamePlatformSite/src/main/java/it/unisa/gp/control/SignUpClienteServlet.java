53b6f2b16cc772ad0a24e22784f94833601a7d95
ciro <cirovitale@pm.me>
2022-07-11 18:04:31 +0200

    Implementazione servlet per signUp dei clienti con relativa jsp

---
+++ gamePlatformSite/src/main/java/it/unisa/gp/control/SignUpClienteServlet.java
@@ -0,0 +1,106 @@
+package it.unisa.gp.control;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import it.unisa.gp.model.DAO.ClientiDS;
+import it.unisa.gp.model.DAO.TelefonoDS;
+import it.unisa.gp.model.bean.ClientiBean;
+import it.unisa.gp.model.bean.TelefonoBean;
+import it.unisa.gp.model.interfaceDS.Clienti;
+import it.unisa.gp.model.interfaceDS.Telefono;
+
+/**
+ * Servlet implementation class SignUpClienteServlet
+ */
+@WebServlet("/SignUpClienteServlet")
+public class SignUpClienteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SignUpClienteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String redirectedPage;
+		
+		String codFis = request.getParameter("codFis");
+		String nome = request.getParameter("nome");
+		String cognome = request.getParameter("cognome");
+		String data = request.getParameter("data");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		String username = request.getParameter("username");
+		String indFatt = request.getParameter("indFatt");
+		String tel1 = request.getParameter("tel1");
+		String tel2 = request.getParameter("tel2");
+		
+		int num = 1;
+		if (tel2 != null) {
+			num++;
+		}
+
+		ClientiBean clienteBean = new ClientiBean(codFis, nome, cognome, LocalDate.parse(data), email, password, username, indFatt);
+		TelefonoBean telBean1 = null;
+		TelefonoBean telBean2 = null;
+		if(num == 1) {
+			telBean1 = new TelefonoBean(Long.valueOf(tel1), codFis);
+		} else {
+			telBean1 = new TelefonoBean(Long.valueOf(tel1), codFis);
+			telBean2 = new TelefonoBean(Long.valueOf(tel2), codFis);
+		}
+		
+		DataSource ds = (DataSource) getServletContext().getAttribute("DataSource");
+		Clienti clDS = new ClientiDS(ds);
+		Telefono telDS = new TelefonoDS(ds);
+		
+		try {
+			clDS.doSave(clienteBean);
+			if(num == 1) {
+			telDS.doSave(telBean1);
+			} else {
+				telDS.doSave(telBean1);
+				telDS.doSave(telBean2);
+			}
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		
+		try {
+			request.getSession().setAttribute("utente", clienteBean);
+			request.getSession().setAttribute("roles", "cliente");
+			
+			redirectedPage = "/protected.jsp";
+			
+		} catch (Exception e) {
+			request.getSession().setAttribute("roles", null);
+			redirectedPage = "/signup-form.jsp";
+		}
+		response.sendRedirect(request.getContextPath() + redirectedPage);
+	}
+}
---
+++ gamePlatformSite/src/main/webapp/signup-form.jsp
@@ -0,0 +1,53 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Registrazione</title>
+</head>
+<body>
+
+<form action="SignUpClienteServlet" method="post"> 
+<fieldset>
+     <legend>Registrazione</legend>
+     <label for="codFis">Codice Fiscale</label>
+     <input id="codFis" type="text" name="codFis" placeholder="codice fiscale" required>  
+     <br>   
+     <label for="nome">Nome</label>
+     <input id="nome" type="text" name="nome" placeholder="nome" required> 
+     <br>
+     <label for="cognome">Cognome</label>
+     <input id="cognome" type="text" name="cognome" placeholder="cognome" required> 
+     <br>   
+     <label for="data">Data</label>
+     <input id="data" type="date" name="data" placeholder="data" required> 
+     <br>     
+     <label for="email">Email</label>
+     <input id="email" type="email" name="email" placeholder="email" required> 
+     <br>
+     <label for="password">Password</label>
+     <input id="password" type="password" name="password" placeholder="password" required> 
+     <br>
+     <label for="username">Username</label>
+     <input id="username" type="text" name="username" placeholder="username" required> 
+     <br>     
+     <label for="indFatt">Indirizzo di fatturazione</label>
+     <input id="indFatt" type="text" name="indFatt" placeholder="indirizzo di fatturazione" required> 
+     <br>
+     
+     <label for="tel1">Numero telefono 1</label>
+     <input id="tel1" type="tel" name="tel1" placeholder="telefono 1" required> 
+     <br>     
+     <label for="tel2">Numero telefono 2</label>
+     <input id="tel2" type="tel" name="tel2" placeholder="telefono 2"> 
+     <br>
+     
+     
+     <input type="submit" value="Registrati"/>
+     <input type="reset" value="Reset"/>
+</fieldset>
+</form>
+
+</body>
+</html>
