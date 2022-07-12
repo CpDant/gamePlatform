3d4cfdc40bb7ab768c61e1b0bfc370bcf4147ade
CpDant <francescopaolo177@gmail.com>
2022-07-13 00:25:30 +0200

    Implementazione funzionalità di aggiunta di videogiochi e
    abbonamenti al carrello

---
+++ gamePlatformSite/src/main/java/it/unisa/gp/control/AddToCartServlet.java
@@ -0,0 +1,50 @@
+package it.unisa.gp.control;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import it.unisa.gp.model.bean.Carrello;
+
+/**
+ * Servlet implementation class AddToCartServlet
+ */
+@WebServlet("/AddToCartServlet")
+public class AddToCartServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddToCartServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Carrello carrello = (Carrello) request.getSession().getAttribute("carrello");
+		String id = request.getParameter("id");
+		if(id.contains(" Pass")) {
+			carrello.addAbb(id);
+		}else {
+			carrello.addVid(id);
+		}
+		
+		response.sendRedirect(request.getContextPath() + "/carrello.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
